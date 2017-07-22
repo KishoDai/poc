c5a816469f999399abb6a652bc4ad8ab23ddb798
Administrator <daijizhang@163.com>
2017-07-22 13:43:09 +0800

    添加大数据元数据模型

--- springboot-poc/src/main/java/org/poc/springboot/schema/impl/AvroSchemaServiceImpl.groovy
+++ springboot-poc/src/main/java/org/poc/springboot/schema/impl/AvroSchemaServiceImpl.groovy
@@ -3,20 +3,25 @@ package org.poc.springboot.schema.impl
 import org.poc.springboot.schema.SchemaService
 import org.springframework.stereotype.Service
 
+import javax.annotation.Resource
+
 /**
  * Created by Administrator on 2017/7/21 0021.
  */
 @Service
 class AvroSchemaServiceImpl implements SchemaService {
 
+    @Resource(name = 'globalMap')
+    private Map globalMap
+
     @Override
     def getSchemas(String queryCondition) {
-        ['schemas', queryCondition]
+        ['schemas', queryCondition, globalMap.test]
     }
 
     @Override
     def getShemaVersions(String queryCondition) {
-        ['schemaVersion', queryCondition]
+        ['schemaVersion', queryCondition, globalMap.test]
     }
 
 }
